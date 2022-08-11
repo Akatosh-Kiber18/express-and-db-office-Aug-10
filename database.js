8726140a01ff0428c195d706665937fe4d0f0c02
Akatosh-Kiber18 <Cemper300@gmail.com>
2022-08-11 11:57:42 +0300

    Update database.js

--- database.js
+++ database.js
@@ -1,8 +1,8 @@
 import pg from 'pg';
 export const pool = new pg.Pool({
-    user: 'todolist_app', //username
+    user: '', //username
     host: '127.0.0.1',
     database: 'postgres',
-    password: '12345678', //password
+    password: '', //password
     port: 5432,
 });
